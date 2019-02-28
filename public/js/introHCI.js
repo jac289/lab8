5d1a67df156449005d54b52f08db2d5dbe053e19
Jaclyn Chin <jac289@ucsd.edu>
2019-02-28 10:28:13 -0800

    thirdpartyAPI lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,18 +10,41 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+ 	 initCamera();
+ 	 initMap();
  	initGestures();
  	initRSVPForm();
 }
 
+$(function(){
+$("div.box").bind("taphold, tapholdHandler");
+function tapHoldHandler(event){
+		$(event.target).addClass("taphold");
+		}
+	});
 // init jQuery gestures  
 function initGestures() {
+	$(function(){
+		$(".judge-img").bind("taphold", tapholdHandler);
+		function tapholdHandler(event){
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix: " + targetIDPrefix);
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
+
 	// add gestures listener here
 }
 
-// init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+	$('#rsvpForm').submit(function(e){
+		e.preventDefault();
+		console.log("submitting form...")
+		var rsvpEmail = $('#rsvpEmail').val();
+		$.post('addRSVP', {rsvpEmail: rsvpEmail }, postCallback);
+	});
+	function postCallback(res){
+		alert("RSVP form successfully submitted!");
+		$('#rsvpEmail').val('');
+	}
 }
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'CdBATjAHbT7xIjJwh39bjCntYL6rTD5G';
+
+	var map = L.mapquest.map('map', {
+		center: [32.88 , -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.878854, -117.235911]).addTo(map);
 }
